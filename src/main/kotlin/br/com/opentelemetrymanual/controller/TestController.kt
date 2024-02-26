289a899c268199e9fab4fca0cf9c7ac615acde6f
Victor Correia <victorcorreia@sas.com.br>
2024-02-26 16:15:08 -0300

    tracer working ok

--- src/main/kotlin/br/com/opentelemetrymanual/controller/TestController.kt
+++ src/main/kotlin/br/com/opentelemetrymanual/controller/TestController.kt
@@ -1,7 +1,9 @@
 package br.com.opentelemetrymanual.controller
 
+import br.com.opentelemetrymanual.openTelemetry
 import br.com.opentelemetrymanual.service.ServiceA
 import br.com.opentelemetrymanual.service.ServiceB
+import io.opentelemetry.api.trace.Tracer
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,18 +14,28 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/test")
 class TestController(
     private val serviceA: ServiceA,
-    private val serviceB: ServiceB
+    private val serviceB: ServiceB,
+    private val tracer: Tracer = openTelemetry().getTracer("TesteController")
 ) {
     @GetMapping()
     fun testOpenTelemetry(@RequestParam(required = false) service: String): Any {
-        if (service == "A") {
-            return serviceA.execute()
-        }
+        val span = tracer.spanBuilder("teste").startSpan()
+        try {
+            span.makeCurrent()
+            if (service == "A") {
+                return serviceA.execute()
+            }
 
-        if (service == "B") {
-            return serviceB.execute()
-        }
+            if (service == "B") {
+                return serviceB.execute()
+            }
 
-        return HttpStatus.OK
+            return HttpStatus.OK
+        } catch (t: Throwable) {
+            span.recordException(t)
+            throw t
+        } finally {
+            span.end()
+        }
     }
 }

ad1ea2636f5e1db03a948195c3b16a6bb8a29510
Victor Correia <victorcorreia@sas.com.br>
2024-02-27 11:56:00 -0300

    trying to send data to new relic

--- src/main/kotlin/br/com/opentelemetrymanual/OpentelemetryManualConfigApplication.kt
+++ src/main/kotlin/br/com/opentelemetrymanual/OpentelemetryManualConfigApplication.kt
@@ -5,11 +5,10 @@ import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator
 import io.opentelemetry.context.propagation.ContextPropagators
 import io.opentelemetry.context.propagation.TextMapPropagator
-import io.opentelemetry.exporter.logging.LoggingMetricExporter
 import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter
 import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter
+import io.opentelemetry.exporter.otlp.http.metrics.OtlpHttpMetricExporter
 import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter
 import io.opentelemetry.sdk.OpenTelemetrySdk
 import io.opentelemetry.sdk.logs.SdkLoggerProvider
 import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor
@@ -37,19 +36,39 @@ fun openTelemetry(): OpenTelemetry {
         .put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build()
 
     val sdkTracerProvider = SdkTracerProvider.builder()
-        .addSpanProcessor(BatchSpanProcessor.builder(OtlpHttpSpanExporter.builder().build()).build())
+        .addSpanProcessor(
+            BatchSpanProcessor.builder(
+                OtlpHttpSpanExporter.builder().setEndpoint("https://otlp.nr-data.net:4318/v1/traces")
+                    .addHeader("api-key", "c775892ce0ef53d2063f627439233e41FFFFNRAL").build()
+            ).build()
+        )
         .setResource(resource)
         .build()
 
     val sdkMeterProvider = SdkMeterProvider.builder()
-        .registerMetricReader(PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().build()).build())
+        .registerMetricReader(
+            PeriodicMetricReader.builder(
+                OtlpHttpMetricExporter.builder().setEndpoint("https://otlp.nr-data.net:4318/v1/metrics")
+                    .addHeader("api-key", "c775892ce0ef53d2063f627439233e41FFFFNRAL").build()
+            ).build()
+        )
         .setResource(resource)
         .build()
 
     val sdkLoggerProvider = SdkLoggerProvider.builder()
-        .addLogRecordProcessor(BatchLogRecordProcessor.builder(OtlpHttpLogRecordExporter.builder().build()).build())
+        .addLogRecordProcessor(BatchLogRecordProcessor.builder(SystemOutLogRecordExporter.create()).build())
         .setResource(resource)
         .build()
+
+//    val sdkLoggerProvider = SdkLoggerProvider.builder()
+//        .addLogRecordProcessor(
+//            BatchLogRecordProcessor.builder(  // https://opentelemetry.io/docs/languages/java/instrumentation/#logrecord-processor
+//                OtlpHttpLogRecordExporter.builder().setEndpoint("https://otlp.nr-data.net")
+//                    .addHeader("api-key", "c775892ce0ef53d2063f627439233e41FFFFNRAL").build()
+//            ).build()
+//        )
+//        .setResource(resource)
+//        .build()
 
     val openTelemetry = OpenTelemetrySdk.builder()
         .setTracerProvider(sdkTracerProvider)
